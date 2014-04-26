7bf8d459eedf7607fb156f3d86179907c3385b79
nikhilrp <nikhilrp@stanford.edu>
2014-04-26 15:45:49 -0700

    Merge branch 'master' of https://github.com/nikhilRP/nobProject
    Conflicts:
    	main.py

--- main.py
+++ main.py
@@ -2,19 +2,22 @@ import requests
 
 
 HEADERS = {'accept': 'application/json'}
-MONARCH_PHENOTYPE_URL = "http://monarchinitiative.org/phenotype/"
-MONARCH_GENEOTYPE_URL = "http://monarchinitiative.org/genotype/"
+MONARCH_DISEASE_URL = "http://monarchinitiative.org/disease/"
 
 
-def search_monarch(term_type, term):
+def search_monarch(disease_ids):
     ''' Searches monarch and returns the results'''
 
-    if term_type == "gene":
-        url = MONARCH_GENEOTYPE_URL
-    else:
-        url = MONARCH_PHENOTYPE_URL
-    response = requests.get(url + term + '.json', headers=HEADERS)
-    return response.json()
+    diseases = []
+    for disease_id in disease_ids:
+        temp_id = disease_id.replace(':', '_')
+        temp_url = MONARCH_DISEASE_URL + temp_id + '/gene_associations.json'
+        try:
+            disease = requests.get(temp_url, headers=HEADERS)
+        except:
+            print temp_id
+        diseases.append(disease.json())
+    return diseases
 
 
 def main():
@@ -23,12 +26,10 @@ def main():
         description="Search multiple databases using this module",
         formatter_class=argparse.RawDescriptionHelpFormatter,
     )
-    parser.add_argument('--term-type', help="Item type")
-    parser.add_argument('--term-name', help="Search term")
+    parser.add_argument('--disease-ids', action='append', help="Search term")
     args = parser.parse_args()
-    term_type = args.term_type
-    term = args.term_name
-    results = search_monarch(term_type, term)
+    disease_ids = args.disease_ids
+    diseases = search_monarch(disease_ids)
 
 
 if __name__ == main():
