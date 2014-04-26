6e961e0b13d6a36c8ce658f501a8ba2998b1a0bd
nikhilrp <nikhilrp@stanford.edu>
2014-04-26 13:38:04 -0700

    Skeleton structure for querying monarch

---
+++ main.py
@@ -0,0 +1,36 @@
+import requests
+
+
+HEADERS = {'accept': 'application/json'}
+MONARCH_PHENOTYPE_URL = "http://monarchinitiative.org/phenotype/"
+MONARCH_GENEOTYPE_URL = "http://monarchinitiative.org/geneotype/"
+
+
+def search_monarch(term_type, term):
+    ''' Searches monarch and returns the results'''
+
+    if term_type == "gene":
+        url = MONARCH_GENEOTYPE_URL
+    else:
+        url = MONARCH_PHENOTYPE_URL
+    response = requests.get(url + term + '.json', headers=HEADERS)
+    return response.json()
+
+
+def main():
+    import argparse
+    parser = argparse.ArgumentParser(
+        description="Search multiple databases using this module",
+        formatter_class=argparse.RawDescriptionHelpFormatter,
+    )
+    parser.add_argument('--term-type', help="Item type")
+    parser.add_argument('--term-name', help="Search term")
+    args = parser.parse_args()
+    term_type = args.term_type
+    term = args.term_name
+
+    return search_monarch(term_type, term)
+
+
+if __name__ == main():
+    main()
