411c41856b69582166d69d8a34ee59035fac40dc
nikhilrp <nikhilrp@stanford.edu>
2014-04-26 16:25:13 -0700

    Changes to the code

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ import requests
 
 HEADERS = {'accept': 'application/json'}
 MONARCH_DISEASE_URL = "http://monarchinitiative.org/disease/"
+MONARCH_GENE_URL = "http://monarchinitiative.org/gene/"
+NCBI_EUTILS = "http://eutils.ncbi.nlm.nih.gov/entrez/eutils/"
 
 
 def search_monarch(disease_ids):
@@ -11,12 +13,26 @@ def search_monarch(disease_ids):
     diseases = []
     for disease_id in disease_ids:
         temp_id = disease_id.replace(':', '_')
-        temp_url = MONARCH_DISEASE_URL + temp_id + '/gene_associations.json'
+        temp_url = MONARCH_DISEASE_URL + temp_id + '.json'
         try:
             disease = requests.get(temp_url, headers=HEADERS)
+            disease = disease.json()
+            if 'gene_associations' in disease:
+                for gene in disease['gene_associations']:
+                    temp_gene_id = gene['gene']['id'].replace(':', '_')
+                    temp_gene_url = MONARCH_GENE_URL + temp_gene_id + '.json'
+                    g = requests.get(temp_gene_url, headers=HEADERS)
+                    g = g.json()
+                    for ref in g['references']:
+                        if ref['source'] == 'Ensembl':
+                            gene['gene']['ensembl_id'] = []
+                            for i in ref['id']:
+                                gene['gene']['ensembl_id'].append(i)
+            del(disease['@context'])
+            del(disease['sim'])
+            diseases.append(disease)
         except:
-            print temp_id
-        diseases.append(disease.json())
+            pass
     return diseases
 
 
